453712c61efc40d723bdb0dab81226703880ecf1
ttreit <14933277+ttreit@users.noreply.github.com>
2020-11-14 10:18:57 -0800

    Change db from local to remote

--- db-mongo/index.js
+++ db-mongo/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost: 27017/chewy',
+mongoose.connect('mongodb://username:password@67.202.40.97:27017/chewy',
   { useNewUrlParser: true, useUnifiedTopology: true })
   .then(console.log('Database connected'))
   .catch((err) => console.log(err));
